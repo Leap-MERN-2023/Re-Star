b5e098b3baff29de23cc992282413a99fb997ecb
BIlguun B <145326196+bil9uun@users.noreply.github.com>
2024-03-15 18:39:29 +0800

    header finish (#16)
    bilguun ==>header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { MainNav } from "./main-nav";
+import { UserNav } from "./user-nav";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="absolute w-[100%] container ">
+      <div className="flex h-16 items-center px-4 bg-transparent justify-evenly">
+        <img className="w-20 h-auto" src="/images/logo.png" />
+        <div>
+          <MainNav className="mx-6" />
+        </div>
+        <div className="flex items-center space-x-4 bg-transparent">
+          <UserNav />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
