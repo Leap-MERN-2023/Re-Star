f51b4df86f901f67eb5b81571efb5a6b711bc87c
Bilguun <bilguunzx011030@gmail.com>
2024-03-22 18:17:29 +0800

    changeUserProfile hiilee

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUserById } from "../controller/user";
+import { changeUserData, getUserById } from "../controller/user";
+import { authenticate } from "../middleware/authenticate";
 
 const router = Router();
 
-router.route("/").get(getUserById);
+router.route("/").get(getUserById).put(authenticate, changeUserData);
 
 export default router;
