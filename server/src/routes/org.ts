98a290d7f4b944f89cb1856f4f3765b1cb517181
BatboldO <145326207+BatboldO@users.noreply.github.com>
2024-03-22 20:37:49 +0800

    delete, update organization (#60)
    Co-authored-by: Batbold Otgonbayar <LP230114@G301-CC0203-3.local>

--- server/src/routes/org.ts
+++ server/src/routes/org.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { addOrg } from "../controller/org";
+import { addOrg, deleteOrg, getOrg, updateOrg } from "../controller/org";
+import { authenticate } from "../middleware/authenticate";
 
 const router = Router();
 
+router.route("/").get(authenticate, getOrg)
 router.route("/add").post(addOrg);
+router.route("/update").put(authenticate, updateOrg);
+router.route("/delete").delete(authenticate, deleteOrg)
 
 export default router;
