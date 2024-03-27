12077e03ad7d7e0dfa40774f5c0adc32f75c2641
BatboldO <145326207+BatboldO@users.noreply.github.com>
2024-03-27 20:52:35 +0800

    fav card (#94)
    Co-authored-by: Batbold Otgonbayar <LP230114@G301-CC0203-3.local>

--- client/src/components/sections/favoriteCard/index.tsx
+++ client/src/components/sections/favoriteCard/index.tsx
@@ -11,51 +11,40 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Badge } from "@/components/ui/badge";
-import { BiPhone } from "@/components/icons";
-import { useRouter } from "next/router";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+
 import { FaStar } from "@/components/icons";
 
 import Checkbox from "@mui/material/Checkbox";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-export function RestaurantCard({ favorite }: any) {
+
+export function FavoriteCard({ favorite }: any) {
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
   return (
-    <div className=" flex justify-center items-center ">
-      <Card className="">
+    <div className="flex flex-row justify-center items-center rounded-lg border-2 border-gray-200 w-[500px] h-[190px] m-10 hover:scale-105">
         <img
           src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8UmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D"
-          className="h-[200px] w-[380px] rounded-lg p-1"
+          className="h-[180px] w-[260px] border-white-200 rounded-lg p-2"
         />
           <CardHeader>
-            <div className="grid grid-cols-2">
-            <CardTitle className="text-xl">{}Restaurant Name</CardTitle>
-            <div className="text-sm flex justify-end">
+            <div className="">
+            <CardTitle className="text-xl">Yuna</CardTitle>
               <Badge className="bg-green-500">
                 4.8
                 <span className="m-1">
                   <FaStar />
                 </span>
               </Badge>
-              </div>
             </div>
-          </CardHeader>
-        <CardContent>
-          <div className="flex flex-cols-2 justify-center items-center gap-2">
             <Label className="text-sm text-gray-400">
               Seoul Gudamj, Ulaanbaatar
             </Label>
-            <Label className="text-sm text-green-500 ">$$$</Label>
-                 <div className="flex justify-end ml-6">
-                  <Checkbox
+          </CardHeader>
+        <CardContent>
+          <div className="flex justify-center items-center ">
+            <div className="flex">
+                <Checkbox
                     {...label}
                     icon={<FaRegHeart className="text-red-500 w-6 h-6 " />}
                     checkedIcon={<FaHeart className="text-red-500 w-6 h-6" />}
@@ -63,9 +52,8 @@ export function RestaurantCard({ favorite }: any) {
                   </div>
                 </div>
         </CardContent>
-      </Card>
+      
     </div>
-
   );
 }
-export default RestaurantCard;
+export default FavoriteCard;
