2066587c6fd8cf9ae434024b6169d80ee1073527
Anna <137128445+anna489@users.noreply.github.com>
2024-03-27 18:21:52 +0800

    add category super admin (#86)
    Co-authored-by: Anar Ganbaatar <LP230138@G301-CC0197.local>

---
+++ client/src/app/super-admin/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import SuperAdmin from "@/components/sections/superAdmin";
+
+const SuperPage = () => {
+  return (
+    <div className="bg-[#fdf4ed]">
+      <div className="mx:auto container">
+        <SuperAdmin />
+      </div>
+    </div>
+  );
+};
+
+export default SuperPage;
--- client/src/context/CategoryProvider.tsx
+++ client/src/context/CategoryProvider.tsx
@@ -2,10 +2,13 @@
 import React, {
   PropsWithChildren,
   createContext,
+  useContext,
   useEffect,
   useState,
 } from "react";
 import myAxios from "@/utils/myAxios";
+import { UserContext } from "./UserProvider";
+import { toast } from "react-toastify";
 
 interface ICategory {
   name: string;
@@ -16,12 +19,15 @@ interface ICategory {
 
 interface ICategoryContext {
   categories: ICategory[];
+  addCategory: (dataForm: any) => {};
 }
 
 export const CategoryContext = createContext({} as ICategoryContext);
 
 const CategoryProvider = ({ children }: PropsWithChildren) => {
   const [categories, setCategories] = useState([]);
+
+  const { token } = useContext(UserContext);
 
   const getCategory = async () => {
     const {
@@ -30,12 +36,25 @@ const CategoryProvider = ({ children }: PropsWithChildren) => {
     setCategories(categories);
   };
 
+  const addCategory = async (dataForm: any) => {
+    console.log("DF", dataForm.get("image"));
+    const {
+      data: { categories },
+    } = await myAxios.post("/category/add", dataForm, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    toast.success("success");
+  };
+
   useEffect(() => {
     getCategory();
   }, []);
 
   return (
-    <CategoryContext.Provider value={{ categories }}>
+    <CategoryContext.Provider value={{ categories, addCategory }}>
       {children}
     </CategoryContext.Provider>
   );
