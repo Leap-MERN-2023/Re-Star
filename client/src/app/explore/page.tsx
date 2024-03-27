df17fd3d25a94700eadd4065dfc709671127b450
Anna <137128445+anna489@users.noreply.github.com>
2024-03-27 20:40:14 +0800

    exlore client (#92)
    Co-authored-by: Anar Ganbaatar <LP230138@G301-CC0197.local>

---
+++ client/src/app/explore/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Explore from "@/components/sections/explore";
+
+const ExplorePage = () => {
+  return (
+    <div className="bg-[#fdf4ed]">
+      <div className="mx:auto container">
+        <Explore />
+      </div>
+    </div>
+  );
+};
+
+export default ExplorePage;
---
+++ client/src/components/sections/explore/categoryList.tsx
@@ -0,0 +1,12 @@
+import { CategoryContext } from "@/context/CategoryProvider";
+import React, { useContext } from "react";
+
+const CategoryList = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="rounded w-[250px] h-[900px] shadow-2xl"></div>
+    </div>
+  );
+};
+
+export default CategoryList;
