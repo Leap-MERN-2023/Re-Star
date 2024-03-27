8fc08d790922d55741bff93da08b8fc322a77060
Bilguun <bilguunzx011030@gmail.com>
2024-03-27 18:05:11 +0800

    review crud

--- server/src/controller/review.ts
+++ server/src/controller/review.ts
@@ -28,3 +28,59 @@ export const getReview = async (
     message: "Get all review successfully",
   });
 };
+
+export const getReviewById = async (
+  req: IReq,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const orgReview = await Review.find({ organization: req.body.orgId });
+    console.log("org.Rev", orgReview);
+    res.status(201).json({ orgReview });
+    res.send({ orgReview });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateReview = async (
+  req: IReq,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { changedReview } = req.body;
+    const findReview = await Review.findByIdAndUpdate(
+      {
+        _id: req.body.reviewId,
+      },
+      {
+        message: changedReview.message,
+        score: changedReview.score,
+      }
+    );
+    findReview?.save();
+    res.status(201).json({
+      message: "Хэрэглэгчийн review амжилттай өөрчиллөө.",
+      changedReview,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteReview = async (
+  req: IReq,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const findReview = await Review.findByIdAndDelete({
+      _id: req.body.reviewId,
+    });
+    res.status(201).json({ message: "Heregchinn review amjilttai ustgagdlaa" });
+  } catch (error) {
+    console.log(error);
+  }
+};
