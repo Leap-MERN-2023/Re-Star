3b0a4bfb5a4e10598bbf03f5d49d0d7215c20751
Batbold Otgonbayar <LP230114@G301-CC0203-3.local>
2024-04-08 17:33:24 +0800

    fix

--- client/src/app/aboutUs/page.tsx
+++ client/src/app/aboutUs/page.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
 import AboutUs from "@/components/sections/aboutUs";
-=======
-import About from "@/components/sections/aboutUs";
->>>>>>> main
 
 const Page = () => {
   return (
     <div className="bg-secondary ">
       <div className="mx:auto container">
-<<<<<<< HEAD
         <AboutUs />
-=======
-        <About />
->>>>>>> main
       </div>
     </div>
   );
--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { ProfileSettings } from "@/components/userSettings";
-=======
-import { ProfileSettings } from "@/components/userProfile";
->>>>>>> main
 import React from "react";
 
 const Page = () => {
