576786210562f056b065dcd99b6ba1df492864d7
Anar Ganbaatar <LP230138@G301-CC0197.local>
2024-04-08 15:49:35 +0800

    design

--- client/src/components/sections/navbar/header/index.tsx
+++ client/src/components/sections/navbar/header/index.tsx
@@ -15,7 +15,7 @@ const Header = () => {
     <div className="bg-secondary w-[100%] mx-auto p-3 ">
       <div className="flex h-16 items-center px-4 bg-transparent justify-evenly   ">
         <img
-          className=" sm:w-6  w-6 md:w-10 h-10 lg:w-12 w-12 "
+          className=" sm:w-6  w-6 md:w-10 h-12 lg:w-12 "
           src="https://scontent.xx.fbcdn.net/v/t1.15752-9/430382737_317224774711146_4452897880078353094_n.png?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ZOlaC83G9A0AX82Df-k&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdXaa2A1hS8QUkM860gGxLhy6V8tVhH0hOsNDUoLz5tyHQ&oe=663486A3"
           onClick={() => router.push("/")}
         />
